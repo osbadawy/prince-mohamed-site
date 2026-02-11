03dcff5a6092869d95dbc83b45049c3f95217b76
Omar Badawy <omar.badawy@hotmail.com>
2026-02-11 13:14:16 +0100

    added hero section

---
+++ src/util/data.ts
@@ -0,0 +1,95 @@
+export const products = [
+  {
+    title: "A BRIGHT FUTURE",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero1.jpg",
+  },
+  {
+    title: "INTERNATIONAL COLLABORATIONS",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero2.jpg",
+  },
+  {
+    title: "LEADERSHIP IN SPORTS",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero3.jpg",
+  },
+ 
+  {
+    title: "PRINCE MOHAMED AL SAUD",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero4.jpg",
+  },
+  {
+    title: "OUR TRANSFORMATIVE POWER",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero5.jpg",
+  },
+  {
+    title: "THE TEAM",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero6.jpg",
+  },
+ 
+  {
+    title: "TECHNOLOGY AND INNOVATION",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero7.jpg",
+  },
+  {
+    title: "A WINNING MINDSET",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero8.jpg",
+  },
+  {
+    title: "YOUTH EMPOWERMENT",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero9.jpg",
+  },
+  {
+    title: "INSPIRING THE WORLD",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero10.jpg",
+  },
+  {
+    title: "PARTNERSHIPS AND COLLABORATIONS",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero11.jpg",
+  },
+ 
+  {
+    title: "A SUPPORTIVE COMMUNITY",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero12.jpg",
+  },
+  {
+    title: "MEDIA TRANSFORMATION",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero13.jpg",
+  },
+  {
+    title: "NURTURING TALENT",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero14.jpg",
+  },
+  {
+    title: "STRIVING FOR EXCELLENCE",
+    link: "https://sttf.sa/news",
+    thumbnail:
+      "./images/heroSection/hero15.jpg",
+  },
+];
